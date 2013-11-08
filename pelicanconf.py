2f351f65445fd74c8b125c845589df2a50275f0f
Jeff Tratner <jeffrey.tratner@gmail.com>
2013-11-08 10:59:05 -0500

    switch theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 AUTHOR = u'Jeff Tratner'
 SITENAME = u'Jeff Tratner'
 SITEURL = 'http://www.jeffreytratner.com/blog'
-THEME = 'tuxlite_zf'
+THEME = 'simple'
 
 TIMEZONE = 'Europe/Paris'
 
